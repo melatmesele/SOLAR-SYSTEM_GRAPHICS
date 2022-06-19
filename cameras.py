90f7297d420e73be674af0c7134a4e8df4123432
melat-mesele <89652408+melat-mesele@users.noreply.github.com>
2022-06-18 23:21:00 -0700

    first

---
+++ cameras.py
@@ -0,0 +1,70 @@
+from pyrr import Vector3, vector, vector3, matrix44
+from math import sin, cos, radians
+
+class Cameras:
+    def __init__(self):
+        self.camera_pos = Vector3([0.0, 4.0, 3.0])
+        self.camera_front = Vector3([0.0, 0.0, -1.0])
+        self.camera_up = Vector3([0.0, 1.0, 0.0])
+        self.camera_right = Vector3([1.0, 0.0, 0.0])
+
+        self.mouse_movement = 0.35
+        self.jaw = -90
+        self.pitch = 0
+
+    def get_view_matrix(self):
+        return matrix44.create_look_at(self.camera_pos, self.camera_pos + self.camera_front, self.camera_up)
+
+    def process_mouse_movement(self, xoffset, yoffset, constrain_pitch=True):
+        xoffset *= self.mouse_movement
+        yoffset *= self.mouse_movement
+
+        self.jaw += xoffset
+        self.pitch += yoffset
+
+        if constrain_pitch:
+            if self.pitch > 45:
+                self.pitch = 45
+            if self.pitch < -45:
+                self.pitch = -45
+
+        self.update_camera_vectors()
+        
+         # Camera method for the WASD movement
+    def process_keyboard(self, direction, velocity):
+        if direction == "FORWARD":
+            self.camera_pos += self.camera_front * velocity
+        if direction == "BACKWARD":
+            self.camera_pos -= self.camera_front * velocity
+        if direction == "LEFT":
+            self.camera_pos -= self.camera_right * velocity
+        if direction == "RIGHT":
+            self.camera_pos += self.camera_right * velocity
+
+    def update_camera_vectors(self):
+        front = Vector3([0.0, 0.0, 0.0])
+        front.x = cos(radians(self.jaw)) * cos(radians(self.pitch))
+        front.y = sin(radians(self.pitch))
+        front.z = sin(radians(self.jaw)) * cos(radians(self.pitch))
+
+        self.camera_front = vector.normalise(front)
+        self.camera_right = vector.normalise(vector3.cross(self.camera_front, Vector3([0.0, 1.0, 0.0])))
+        self.camera_up = vector.normalise(vector3.cross(self.camera_right, self.camera_front))
+
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
